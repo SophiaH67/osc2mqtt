b838fa064616ca9a294385b1fe78c99dddc41747
marnixah <marnix@roboco.dev>
2023-02-25 02:56:30 +0100

    working after refactor

--- src/homeassistant.rs
+++ src/homeassistant.rs
@@ -2,17 +2,37 @@ use async_osc::{OscMessage, OscType};
 use paho_mqtt::{AsyncClient, Message};
 use serde_json::json;
 
-struct OscToHass {
-    osc_type: OscType,
+#[derive(Hash, Clone)]
+pub struct HassEntity {
     hass_name: String,
     sensor_type: String,
-    hass_device_class: String,
+    unique_id: String,
+    hass_device_class: Option<String>,
     state_topic: String,
     command_topic: String,
 }
 
-impl OscToHass {
-    fn new(osc_type: OscType, hass_name: &str) -> Self {
+impl PartialEq for HassEntity {
+    fn eq(&self, other: &Self) -> bool {
+        self.hass_name == other.hass_name
+    }
+}
+
+impl Eq for HassEntity {}
+
+impl HassEntity {
+    fn new(osc_message: &OscMessage) -> Self {
+        let osc_type = &osc_message.args[0];
+        // Get the last non-empty segment of the OSC address
+        let osc_address = osc_message.addr.as_str().to_owned();
+        let osc_address_segments: Vec<&str> = osc_address.split("/").collect();
+        let last_segment = osc_address_segments
+            .iter()
+            .rev()
+            .find(|s| !s.is_empty())
+            .unwrap();
+        let hass_name = "Osc".to_owned() + &last_segment.to_string();
+
         let hass_sensor_type = match osc_type {
             OscType::Bool(_) => "switch".to_string(),
             OscType::Float(_) => "number".to_string(),
@@ -20,24 +40,24 @@ impl OscToHass {
             _ => panic!("Unsupported OSC type"),
         };
         let hass_device_class = match osc_type {
-            OscType::Bool(_) => "switch".to_string(),
-            OscType::Float(_) => "None".to_string(),
-            OscType::Int(_) => "None".to_string(),
+            OscType::Bool(_) => Some("switch".to_string()),
+            OscType::Float(_) => None,
+            OscType::Int(_) => None,
             _ => panic!("Unsupported OSC type"),
         };
 
-        OscToHass {
-            osc_type: osc_type.clone(),
-            hass_name: hass_name.to_string(),
+        HassEntity {
+            unique_id: "osc.".to_owned() + &osc_address_segments.join("_"),
+            hass_name: hass_name.clone(),
             sensor_type: hass_sensor_type.clone(),
-            hass_device_class,
+            hass_device_class: hass_device_class,
             state_topic: format!("homeassistant/{}/{}/state", hass_sensor_type, hass_name),
             command_topic: format!("homeassistant/{}/{}/set", hass_sensor_type, hass_name),
         }
     }
 }
 
-async fn register_device(osc_to_hass_mapping: &OscToHass, client: &AsyncClient) {
+async fn register_device(osc_to_hass_mapping: &HassEntity, client: &AsyncClient) {
     let hass_config_topic = format!(
         "homeassistant/{}/{}/config",
         osc_to_hass_mapping.sensor_type, osc_to_hass_mapping.hass_name
@@ -48,40 +68,12 @@ async fn register_device(osc_to_hass_mapping: &OscToHass, client: &AsyncClient) 
         "device_class": osc_to_hass_mapping.hass_device_class,
         "state_topic": osc_to_hass_mapping.state_topic,
         "command_topic": osc_to_hass_mapping.command_topic,
+        "object_id": osc_to_hass_mapping.unique_id,
+        "suggested_area": "Osc",
     });
 
     let message = Message::new(hass_config_topic, hass_config.to_string(), 0);
     client.publish(message).await.unwrap();
-}
-
-pub(crate) async fn handle_message(message: OscMessage, client: &AsyncClient) -> Result<(), &str> {
-    let osc_address = message.addr.as_str().to_owned();
-    let osc_address_copy = osc_address.to_owned();
-    let osc_args = message.args.to_owned();
-
-    // Get the last non-empty segment of the OSC address
-    let osc_address_segments: Vec<&str> = osc_address_copy.split("/").collect();
-    let last_segment = osc_address_segments
-        .iter()
-        .rev()
-        .find(|s| !s.is_empty())
-        .unwrap();
-
-    // Create a mapping from OSC address to Home Assistant device
-    let osc_to_hass_mapping = OscToHass::new(osc_args[0].to_owned(), last_segment);
-
-    // Register the device with Home Assistant
-    register_device(&osc_to_hass_mapping, client).await;
-
-    // If we have a mapping and the OSC message has the correct number of arguments, publish the state
-    let message = Message::new(
-        &osc_to_hass_mapping.state_topic,
-        osc_arg_to_string(&osc_args[0]),
-        0,
-    );
-    client.publish(message).await.unwrap();
-
-    Ok(())
 }
 
 fn osc_arg_to_string(osc_arg: &OscType) -> String {
@@ -97,4 +89,36 @@ fn osc_arg_to_string(osc_arg: &OscType) -> String {
         OscType::Int(value) => value.to_string(),
         _ => panic!("Unsupported OSC type"),
     }
+}
+
+pub(crate) async fn get_or_register_mapping(
+    message: &OscMessage,
+    client: &AsyncClient,
+    mappings: &bimap::BiMap<String, HassEntity>,
+) -> HassEntity {
+    let osc_address = message.addr.as_str().to_owned();
+
+    // Check if the OSC address is already mapped to a Home Assistant device
+    if mappings.contains_left(&osc_address) {
+        return mappings.get_by_left(&osc_address).unwrap().to_owned();
+    }
+
+    // Create a mapping from OSC address to Home Assistant device
+    let osc_to_hass_mapping = HassEntity::new(&message);
+
+    // Register the device with Home Assistant
+    register_device(&osc_to_hass_mapping, client).await;
+
+    osc_to_hass_mapping
+}
+
+pub(crate) async fn update_entity_state(
+    message: &OscMessage,
+    client: &AsyncClient,
+    hass_entity: &HassEntity,
+) {
+    let osc_args = message.args.to_owned();
+
+    let message = Message::new(&hass_entity.state_topic, osc_arg_to_string(&osc_args[0]), 0);
+    client.publish(message).await.unwrap();
 }
--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,19 @@
 use async_osc::{OscPacket, OscSocket, Result};
 use async_std::stream::StreamExt;
+use bimap::BiMap;
 use paho_mqtt as mqtt;
 mod homeassistant;
 
 #[async_std::main]
 async fn main() -> Result<()> {
+    let mut mappings: BiMap<String, homeassistant::HassEntity> = BiMap::new();
+
     let client = mqtt::AsyncClient::new("tcp://192.168.67.85:1883").unwrap();
     let conn_opts = mqtt::ConnectOptionsBuilder::new()
         .keep_alive_interval(std::time::Duration::from_secs(20))
         .clean_session(true)
         .finalize();
     client.connect(conn_opts).await.unwrap();
-
-    println!("Registered devices with Home Assistant");
 
     let mut socket = OscSocket::bind("127.0.0.1:9019").await?;
     println!("Listening on {}", socket.local_addr()?);
@@ -22,10 +23,14 @@ async fn main() -> Result<()> {
         match packet {
             OscPacket::Bundle(_) => {}
             OscPacket::Message(message) => {
-                // Print the address and the arguments
-                homeassistant::handle_message(message, &client)
-                    .await
-                    .unwrap();
+                let hass_entity =
+                    homeassistant::get_or_register_mapping(&message, &client, &mappings).await;
+
+                if !mappings.contains_left(&message.addr) {
+                    mappings.insert(message.addr.clone(), hass_entity.clone());
+                }
+
+                homeassistant::update_entity_state(&message, &client, &hass_entity).await;
             }
         }
     }
